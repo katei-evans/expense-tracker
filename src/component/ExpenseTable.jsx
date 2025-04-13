a1dec1443c77f56408c64c130feb389d3b18c9d9
Katei <kateievans28@gmail.com>
2025-04-13 17:50:31 +0300

    updated-css-added -table-form

--- src/component/ExpenseTable.jsx
+++ src/component/ExpenseTable.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const Tableexpense = () => {
+function ExpenseTable(props) {
   return (
-    <div>
-      
-    </div>
-  )
+    <table className="expense-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount</th>
+          <th>Date</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.expenses.map(function (expense) {
+          return (
+            <tr key={expense.id}>
+              <td>{expense.name}</td>
+              <td>{expense.description}</td>
+              <td>{expense.category}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.date}</td>
+              <td>
+                <button
+                  className="delete-button"
+                  onClick={function () {
+                    props.onDelete(expense.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
-export default Tableexpense
+export default ExpenseTable;
